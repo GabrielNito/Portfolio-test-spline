8beeb6226f241d7e18293f3d3dc2cce90d62215d
Gabriel Nito <nito.gabriel22@gmail.com>
2024-03-07 15:44:45 -0300

    feature: 3d objects

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,23 +1,29 @@
+import Spline from "@splinetool/react-spline";
+
 const Content_Image = () => {
   return (
     <section id="content_image">
       <div className="content_image_container">
+        <Spline scene="https://prod.spline.design/F7WDKkyfjblqGZpa/scene.splinecode" />
         <div className="content_container">
           <h1>Content</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos minus
+            esse qui quasi reiciendis excepturi quaerat repudiandae nobis
+            dolorum? Laboriosam.
+          </p>
         </div>
-        <img src="/JavaScript.png" alt="" />
       </div>
       <div className="content_image_container">
-        <img src="/JavaScript.png" alt="" />
         <div className="content_container">
           <h1>Content</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos minus
+            esse qui quasi reiciendis excepturi quaerat repudiandae nobis
+            dolorum? Laboriosam.
+          </p>
         </div>
-      </div>
-      <div className="content_image_container">
-        <div className="content_container">
-          <h1>Content</h1>
-        </div>
-        <img src="/JavaScript.png" alt="" />
+        <Spline scene="https://prod.spline.design/73N71Qgl8ZoFr2os/scene.splinecode" />
       </div>
     </section>
   );
