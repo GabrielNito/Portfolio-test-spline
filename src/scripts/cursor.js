d40dc9472067c6471fa41f86a9fd811914164dd6
Gabriel Nito <nito.gabriel22@gmail.com>
2024-03-11 19:14:54 -0300

    fix: cursor

--- src/scripts/cursor.js
+++ src/scripts/cursor.js
@@ -1,14 +1,20 @@
 import { mobileDetect } from "./mobileDetect";
 
 export default function cursor() {
-    function cursor_() {
+    const animateCursor = () => {
         document.body.setAttribute("style", "cursor: none")
         const cursorDot = document.querySelector("#cursor")
         cursorDot.style.display = "block"
-        let previousPosition = { x: 0, y: 0 }
+
         onmousemove = function (e) {
-            cursorDot.style.transform = `translate(${e.x}px, ${e.y}px)`
+            const keyframes = { transform: `translate(${e.x}px, ${e.y}px)` }
+            cursorDot.style.transform = keyframes.transform
+
+            cursorDot.animate(keyframes, {
+                duration: 300,
+                fill: "forwards"
+            });
         }
     }
-    mobileDetect(cursor_)
+    mobileDetect(animateCursor)
 }
